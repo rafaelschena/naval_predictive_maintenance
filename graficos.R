25ea715025124cd6f0b55bfe6a7e4605c5e0a489
rafaelschena <rafaelschena@gmail.com>
2022-05-05 22:04:28 -0300

    Inclusao do teste da hipótese 1 no relatório, gráficos feitos em R

---
+++ graficos.R
@@ -0,0 +1,33 @@
+# Gráfico de evolução dos modelos pelos cenários
+
+library(ggplot2)
+library(ggrepel)
+
+df_comp <- read.csv('compressor_cenarios.csv')
+
+# Gráfico para o compressor
+p1 <- ggplot(df_comp, aes(Cenário, Acuracia.Recall, group = Algoritmo, colour = Algoritmo))
+
+p2 <- p1 + geom_line(size = 1) + theme_bw() +  theme(legend.position="none")
+
+(p3 <- p2 + geom_text_repel(data = df_comp[df_comp$Cenário==1,], aes(label = Algoritmo, 
+                                                               hjust = 1, vjust = 1))
+          + ggtitle("Evolução de Acurácia x Recall_Emerg Compressor")
+          + scale_x_continuous(limits = c(0, 2), n.breaks = 3)
+          + scale_y_continuous(limits = c(0, 1), n.breaks = 10)
+  )
+
+# Gráfico para a turbina
+
+df_turb <- read.csv('turbina_cenarios.csv')
+
+q1 <- ggplot(df_comp, aes(Cenário, Acuracia.Recall, group = Algoritmo, colour = Algoritmo))
+
+q2 <- q1 + geom_line(size = 1) + theme_bw() +  theme(legend.position="none")
+
+(q3 <- q2 + geom_text_repel(data = df_comp[df_comp$Cenário==1,], aes(label = Algoritmo, 
+                                                                     hjust = 1, vjust = 1))
+  + ggtitle("Evolução de Acurácia x Recall_Emerg Compressor")
+  + scale_x_continuous(limits = c(0, 2), n.breaks = 3)
+  + scale_y_continuous(limits = c(0, 1), n.breaks = 10)
+)
