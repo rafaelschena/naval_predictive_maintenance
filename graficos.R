b1712a13e3cbe77069066839c8b9f49935d22bce
rafaelschena <rafaelschena@gmail.com>
2022-05-06 21:32:45 -0300

    Relatório final

--- graficos.R
+++ graficos.R
@@ -21,13 +21,13 @@ p2 <- p1 + geom_line(size = 1) + theme_bw() +  theme(legend.position="none")
 
 df_turb <- read.csv('turbina_cenarios.csv')
 
-q1 <- ggplot(df_comp, aes(Cenário, Acuracia.Recall, group = Algoritmo, colour = Algoritmo))
+q1 <- ggplot(df_turb, aes(Cenário, Acuracia.Recall, group = Algoritmo, colour = Algoritmo))
 
 q2 <- q1 + geom_line(size = 1) + theme_bw() +  theme(legend.position="none")
 
 (q3 <- q2 + geom_text_repel(data = df_comp[df_comp$Cenário==1,], aes(label = Algoritmo, 
                                                                      hjust = 1, vjust = 1))
-  + ggtitle("Evolução de Acurácia x Recall_Emerg Compressor")
+  + ggtitle("Evolução de Acurácia x Recall_Emerg Turbina")
   + scale_x_continuous(limits = c(0, 2), n.breaks = 3)
   + scale_y_continuous(limits = c(0, 1), n.breaks = 10)
 )
